d62e30d45a8ea07c7ab8f9381d082065c8fb867c
martin <martin@paredesvazquez.mx>
2023-11-05 14:15:53 -0600

    Se agregan vistas de registro y feed

---
+++ modulos/feed/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>***Avisos***</title>
+    <link rel="stylesheet" href="./estilos/feed.css" />
+</head>
+
+<body>
+    <ul>
+        <li><a class="active" href="#avisos">Avisos</a></li>
+        <li><a href="#tutor">Tutor</a></li>
+        <li><a href="#alumnos">Alumnos</a></li>
+        <li><a href="#padres">Padres</a></li>
+        <li style="float: right">
+            <a class="active" style="cursor: pointer" onclick="window.location.href='index.html';">Salir</a>
+        </li>
+    </ul>
+
+    <h2 style="text-align: center">Avisos</h2>
+
+    <div class="container">
+        <div class="mySlides">
+            <div class="numbertext">1 / 6</div>
+            <img src="https://www.w3schools.com/howto/img_woods_wide.jpg" style="width: 100%" />
+        </div>
+
+        <div class="mySlides">
+            <div class="numbertext">2 / 6</div>
+            <img src="https://www.w3schools.com/howto/img_5terre_wide.jpg" style="width: 100%" />
+        </div>
+
+        <div class="mySlides">
+            <div class="numbertext">3 / 6</div>
+            <img src="https://www.w3schools.com/howto/img_mountains_wide.jpg" style="width: 100%" />
+        </div>
+
+        <div class="mySlides">
+            <div class="numbertext">4 / 6</div>
+            <img src="https://www.w3schools.com/howto/img_lights_wide.jpg" style="width: 100%" />
+        </div>
+
+        <div class="mySlides">
+            <div class="numbertext">5 / 6</div>
+            <img src="https://www.w3schools.com/howto/img_nature_wide.jpg" style="width: 100%" />
+        </div>
+
+        <div class="mySlides">
+            <div class="numbertext">6 / 6</div>
+            <img src="https://www.w3schools.com/howto/img_snow_wide.jpg" style="width: 100%" />
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+
+        <div class="caption-container">
+            <p id="caption"></p>
+        </div>
+
+        <div class="row">
+            <div class="column">
+                <img class="demo cursor" src="https://www.w3schools.com/howto/img_woods.jpg" style="width: 100%" onclick="currentSlide(1)" alt="Semana Santa" />
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="https://www.w3schools.com/howto/img_5terre.jpg" style="width: 100%" onclick="currentSlide(2)" alt="Fin de Ciclo" />
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="https://www.w3schools.com/howto/img_mountains.jpg" style="width: 100%" onclick="currentSlide(3)" alt="Inicio de Ciclo" />
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="https://www.w3schools.com/howto/img_lights.jpg" style="width: 100%" onclick="currentSlide(4)" alt="Día de Muertos" />
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="https://www.w3schools.com/howto/img_nature.jpg" style="width: 100%" onclick="currentSlide(5)" alt="Navidad" />
+            </div>
+            <div class="column">
+                <img class="demo cursor" src="https://www.w3schools.com/howto/img_snow.jpg" style="width: 100%" onclick="currentSlide(6)" alt="Inscripciones" />
+            </div>
+        </div>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ modulos/registro/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <title>Registro</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./estilos/styles.css">
+    <link rel="stylesheet" href="./estilos/registro.css">
+</head>
+
+<body>
+
+    <form class="modal-content">
+        <div class="container">
+
+            <div class="input-container">
+                <i class="fa fa-user icon"></i>
+                <input class="input-field" type="text" placeholder="Usuario" name="usrnm">
+            </div>
+
+            <div class="input-container">
+                <i class="fa fa-envelope icon"></i>
+                <input class="input-field" type="text" placeholder="Correo" name="email">
+            </div>
+
+            <div class="input-container">
+                <i class="fa fa-key icon"></i>
+                <input class="input-field" type="password" placeholder="Contraseña" name="psw">
+            </div>
+
+            <button type="button" onclick="window.location.href='feed.html';">Registrarme</button>
+        </div>
+
+        <div class="container">
+            <button type="button" class="cancelbtn" onclick="window.location.href='index.html';">Ingresar, ya tengo
+                cuenta</button>
+        </div>
+    </form>
+
+</body>
+
+</html>
