b84c7f38d2578ab064e54caf778cec0ee3937410
martin <martin@paredesvazquez.mx>
2023-11-05 17:08:59 -0600

    Ajuste en pantalla home

--- index.php
+++ index.php
@@ -6,36 +6,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Ingresar</title>
     <link rel="stylesheet" href="./estilos/styles.css" />
-    <link rel="stylesheet" href="./estilos/login.css" />
 </head>
 
-<body>
-    <form class="modal-content">
-        <div class="imgcontainer">
-            <img src="https://www.w3schools.com/howto/img_avatar2.png" alt="Avatar" class="avatar" />
+<body style="background-color: #ffffff;">
+    <div>
+        <div style="padding: 20px; text-align: center;">
+            <img src="./assets/logo_cecyte.jpg" alt="Logo" class="logo-cecyte-home" />
         </div>
 
-        <div class="container">
-            <label for="uname"><b>Usuario</b></label>
-            <input type="text" placeholder="Ingresa tu usuario" name="uname" required />
-
-            <label for="psw"><b>Contraseña</b></label>
-            <input type="password" placeholder="Ingresa tu contraseña" name="psw" required />
-
-            <button type="button" onclick="window.location.href='feed.html';">
+        <div class="container-button-home">
+            <button type="button" class="cancelbtn2" onclick="window.location.href='./modulos/login/index.php';">
                 Ingresar
             </button>
-            <label>
-                <input type="checkbox" checked="checked" name="remember" /> Recordarme
-            </label>
-        </div>
-
-        <div class="container">
-            <button type="button" class="cancelbtn" onclick="window.location.href='registro.html';">
-                Registrarme, no tengo cuenta
+            <button type="button" class="cancelbtn2" onclick="window.location.href='./modulos/registro/index.php';">
+                Registrarme
             </button>
         </div>
-    </form>
+    </div>
 </body>
 
 </html>
