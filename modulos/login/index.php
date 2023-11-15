e839d26996a4e5677cb763ec27553cfb7d042ecd
martin <martin@paredesvazquez.mx>
2023-11-15 13:18:30 -0600

    Ajustes de estilos en pantalla de login y validación de campos en formulario

--- modulos/login/index.php
+++ modulos/login/index.php
@@ -5,29 +5,31 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Ingresar</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="../../estilos/styles.css" />
     <link rel="stylesheet" href="../../estilos/login.css" />
 </head>
 
 <body>
-    <form class="modal-content">
+    <form class="modal-content" action="../feed/index.php">
         <div class="imgcontainer">
-            <img src="../../assets/img_avatar2.png" alt="Avatar" class="avatar" />
+            <i class="fa fa-user avatar"></i>
         </div>
 
         <div class="container">
-            <label for="email"><b>Correo</b></label>
-            <input type="text" placeholder="Ingresa tu correo" name="email" required />
+            <div class="input-container">
+                <i class="fa fa-envelope icon"></i>
+                <input class="input-field" type="email" placeholder="Correo" name="email" required>
+            </div>
 
-            <label for="psw"><b>Contraseña</b></label>
-            <input type="password" placeholder="Ingresa tu contraseña" name="psw" required />
+            <div class="input-container">
+                <i class="fa fa-key icon"></i>
+                <input id="pwd" name="pwd" class="input-field" type="password" placeholder="Contraseña" pattern="^\S{6,}$" oninput="checkPassword()" maxlength="8" required>
+            </div>
 
-            <button type="button" onclick="window.location.href='../feed/index.php';">
+            <button type="submit">
                 Ingresar
             </button>
-            <label>
-                <input type="checkbox" checked="checked" name="remember" /> Recordarme
-            </label>
         </div>
 
         <div class="container">
