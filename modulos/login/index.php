4cfa1eef83c64e43150939e30da8b76f585468fe
martin <martin@paredesvazquez.mx>
2023-11-15 22:02:21 -0600

    Ajustes look & feel en pantalla de Login

--- modulos/login/index.php
+++ modulos/login/index.php
@@ -11,33 +11,37 @@
 </head>
 
 <body>
-    <form class="modal-content" action="../feed/index.php">
-        <div class="imgcontainer">
-            <i class="fa fa-user avatar"></i>
+    <div class="wrapper">
+        <div class="logincontainer ">
+            <form class="modal-content" action="../feed/index.php">
+                <div class="imgcontainer">
+                    <i class="fa fa-user avatar"></i>
+                </div>
+
+                <div class="container">
+                    <div class="input-container">
+                        <i class="fa fa-envelope icon"></i>
+                        <input class="input-field" type="email" placeholder="Correo" name="email" required>
+                    </div>
+
+                    <div class="input-container">
+                        <i class="fa fa-key icon"></i>
+                        <input id="pwd" name="pwd" class="input-field" type="password" placeholder="Contraseña" pattern="^\S{6,}$" oninput="checkPassword()" maxlength="8" required>
+                    </div>
+
+                    <button type="submit">
+                        Ingresar
+                    </button>
+                </div>
+
+                <div class="container">
+                    <button type="button" class="cancelbtn" onclick="window.location.href='../registro/index.php';">
+                        Registrarme, no tengo cuenta
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <div class="container">
-            <div class="input-container">
-                <i class="fa fa-envelope icon"></i>
-                <input class="input-field" type="email" placeholder="Correo" name="email" required>
-            </div>
-
-            <div class="input-container">
-                <i class="fa fa-key icon"></i>
-                <input id="pwd" name="pwd" class="input-field" type="password" placeholder="Contraseña" pattern="^\S{6,}$" oninput="checkPassword()" maxlength="8" required>
-            </div>
-
-            <button type="submit">
-                Ingresar
-            </button>
-        </div>
-
-        <div class="container">
-            <button type="button" class="cancelbtn" onclick="window.location.href='../registro/index.php';">
-                Registrarme, no tengo cuenta
-            </button>
-        </div>
-    </form>
+    </div>
 </body>
 
 </html>
